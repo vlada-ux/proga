3847e3fef4b18b292e8fbd689aeb151b98e51035
vlada-ux <pirooovavlada@gmail.com>
2025-09-24 10:11:49 +0200

    Update main.cpp

--- LAB 1/main.cpp
+++ LAB 1/main.cpp
@@ -2,20 +2,19 @@
 #include <cmath>    
 
 int main() {
+    setlocale(LC_ALL,"Russian");
     const double PI = 13.1415;
     double radius;
-    std::cout << "радиус сферы: ";
-    std::cin >> radius;
+    std::cin >> radius; //т.к значение может быть и дробным
+    std::cout << "размер double - " << sizeof(radius) * 8 << " бит, минимальное значение - 0, максимальное - " << UINT_MAX << std::endl;
 
-    double s;
-    double f;
+    double sfera = (4.0 / 3.0) * PI * std::pow(radius, 3); //т.к в формуле есть PI
+    double plo = 4.0 * PI * std::pow(radius, 2); //т.к в формуле есть PI
 
-    s = (4.0 / 3.0) * PI * std::pow(radius, 3);
+    std::cout << "объем сферы: " << sfera << std::endl;
+    std::cout << "площадь поверхности сферы: " << plo << std::endl;
 
-    f = 4.0 * PI * std::pow(radius, 2);
-
-
-    std::cout << "результат: " << std::endl;
-
+    std::cout << "размер double - " << sizeof(sfera) * 8 << " бит, минимальное значение - " << DBL_MIN << ", максимальное - " << DBL_MAX;
+    std::cout << "размер double - " << sizeof(plo) * 8 << " бит, минимальное значение - " << DBL_MIN << ", максимальное - " << DBL_MAX;
     return 0; 
 }
